8b9d590f8e101e0fb3a2eda4863a6d58f4b92b4c
savageafrika <113559289+savageafrika@users.noreply.github.com>
2023-11-14 20:17:14 +0100

    GST 107 - Chapter 1

---
+++ script.js
@@ -0,0 +1,130 @@
+
+
+ 
+   //Questions array
+   let questions = [
+    {
+         number:1,
+         question: " Philosophy can be discussed in two senses namely ?",
+         answer : "Populist sense and professional sense",
+         options : [
+                          "Worldly sense and Metaphysical sense",
+                          "Scientific sense and philosophical sense",
+                          "Populist sense and professional sense",
+                          "Greek sense and African sense"
+                        ]
+    
+    },
+    {
+         number:2,
+         question: " The populist notion expresses a __ understanding of philosophy ?",
+         answer : "Layman",
+         options : [
+                          "Worldly",
+                          "Layman",
+                          "Greek",
+                          "General"  
+                          ]
+    
+    },
+   {
+         number:3,
+         question: "In layman terms philosophy is ?",
+         answer : "An attitude to life",
+         options : [ 
+                          "An attitude to life",
+                          "The truth and the life",
+                          "A pattern of argument",
+                          "A branch of study"
+                          ]
+    
+    },
+    {
+         number:4,
+         question: "The earliest definition of philosophy is the ",
+         answer : "Etymological definition",
+         options : [
+                          "Layman definition",
+                          "Critical definition",
+                          "Professional definition",
+                          "Etymological definition"
+                          ]
+    }, 
+    {
+         number:5,
+         question: "The word Philosophy stems from two ___ words",
+         answer : "Greek",
+         options : [ 
+                          "Latin",
+                          "Greek",
+                          "French",
+                          "Hebrew"
+                       ]
+    },
+    {
+         number:6,
+         question: "In the etymology of philosophy, philos and Sophia means",
+         answer : "Love and wisdom",
+         options : [
+                          "Argument and thought",
+                          "Criticism and argument",
+                          "Love and wisdom",
+                          "Criticism and wisdom"
+                       ]
+    },
+    {
+         number:7,
+         question: "Who defined philosophy in terms of essence",
+         answer : "Aristotle",
+         options : [
+                          "Plato",
+                          "Socrates",
+                          "Aristotle",
+                          "Thales"
+                       ]
+    },
+     {
+         number:8,
+         question: "Who defined philosophy as the science of idea",
+         answer : "Plato",
+         options : [
+                         "Plato",
+                         "Socrates",
+                         "Aristotle",
+                         "Thales"
+                       ]
+    },
+    {
+         number:9,
+         question: "Western philosophy started with which philosopher",
+         answer : "Thales",
+         options : [
+                         "Plato",
+                         "Socrates",
+                         "Aristotle",
+                         "Thales"
+                       ]
+    },
+    {
+         number:10,
+         question: "John Dewey sees philosophy as",
+         answer : "Criticism of criticisms",
+         options : [
+                         "Criticism of criticisms",
+                         "Argument on argument",
+                         "Thinking on thinking",
+                         "Logic of thinking"
+                       ]
+    },
+    {
+         number:11,
+         question: "___ defines philosophy as the Logical clarification of language.",
+         answer : "Ludwig Wittgenstein",
+         options : [
+                         "Gottlob Frege",
+                         "Ludwig Wittgenstein",
+                         "Alfred North Whitehead",
+                         "Charles Sanders"
+                       ]
+    }
+     ];    
